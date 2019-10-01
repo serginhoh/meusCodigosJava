aa08a4a8882f49d9a1058c29ab76d0e140c0352c
Logon Aluno <logonaluno@local.com>
2019-10-01 20:56:40 -0300

    Teste

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,14 @@
 public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
+		
+		//int numero =1;
+		//boolean condicao = true;
+		//float altura = 1.8F;
+		//double largura = 200;
+		//int danilo = 3;
+		//int resultado = numero % danilo;
+		
+		//System.out.println(resultado); 
 	}
 }
---
+++ HelloWorld/src/TestaPessoa.java
@@ -0,0 +1,14 @@
+
+public class TestaPessoa {
+	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa();
+		p1.idade = 12;
+		
+		System.out.println(p1.idade);
+		
+		Pessoa p2 = new Pessoa();
+		p2.nome = "Danilo";
+		
+		System.out.println(p2.nome);
+	}
+}

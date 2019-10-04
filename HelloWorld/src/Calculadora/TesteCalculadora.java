01f890ea5fa1af59114d41339235eab74d9f0a31
Sergio Henrique P. Oliveira <serginho@linx.com.br>
2019-10-04 12:45:23 -0300

    Calculadora

--- HelloWorld/src/Calculadora/TesteCalculadora.java
+++ HelloWorld/src/Calculadora/TesteCalculadora.java
@@ -7,10 +7,10 @@ public class TesteCalculadora {
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 
-		//Soma
-		 String valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Soma", JOptionPane.PLAIN_MESSAGE);
-		 String valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Soma", JOptionPane.PLAIN_MESSAGE);
-		 System.out.println("Soma : (" + valor1 + " + " + valor2  + ") -> " + calc.somar(Float.parseFloat(valor1), Float.parseFloat(valor2)));
+		//Adição
+		 String valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Adição", JOptionPane.PLAIN_MESSAGE);
+		 String valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Adição", JOptionPane.PLAIN_MESSAGE);
+		 System.out.println("Adição : (" + valor1 + " + " + valor2  + ") -> " + calc.somar(Float.parseFloat(valor1), Float.parseFloat(valor2)));
 		
 		 //Subtração
 		 valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Subtração", JOptionPane.PLAIN_MESSAGE);

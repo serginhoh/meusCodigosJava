f2f03909c7fe1635bf5877c15d619ec10d79c464
Logon Aluno <logonaluno@local.com>
2019-10-31 19:16:02 -0300

    update

--- HelloWorld/src/Calculadora/TesteCalculadora.java
+++ HelloWorld/src/Calculadora/TesteCalculadora.java
@@ -7,24 +7,27 @@ public class TesteCalculadora {
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 
+		 String valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Valor", JOptionPane.PLAIN_MESSAGE);
+		 String valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Valor", JOptionPane.PLAIN_MESSAGE);
+		
 		//Adição
-		 String valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Adição", JOptionPane.PLAIN_MESSAGE);
-		 String valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Adição", JOptionPane.PLAIN_MESSAGE);
+		 //String valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Adição", JOptionPane.PLAIN_MESSAGE);
+		 //String valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Adição", JOptionPane.PLAIN_MESSAGE);
 		 System.out.println("Adição : (" + valor1 + " + " + valor2  + ") -> " + calc.somar(Float.parseFloat(valor1), Float.parseFloat(valor2)));
 		
 		 //Subtração
-		 valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Subtração", JOptionPane.PLAIN_MESSAGE);
-		 valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Subtração", JOptionPane.PLAIN_MESSAGE);
+		 //valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Subtração", JOptionPane.PLAIN_MESSAGE);
+		 //valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Subtração", JOptionPane.PLAIN_MESSAGE);
 		 System.out.println("Subtração : (" + valor1 + " - " + valor2 + ") -> " +  calc.subtrair(Float.parseFloat(valor1), Float.parseFloat(valor2)));
 		 
 		 //Divisão
-		 valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Divisão", JOptionPane.PLAIN_MESSAGE);
-		 valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Divisão", JOptionPane.PLAIN_MESSAGE);
+		 //valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Divisão", JOptionPane.PLAIN_MESSAGE);
+		 //valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Divisão", JOptionPane.PLAIN_MESSAGE);
 		 System.out.println("Divisão : (" + valor1 + " / " + valor2 + ") -> " + calc.dividir(Float.parseFloat(valor1), Float.parseFloat(valor2)));
 
 		 //Multiplicação
-		 valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Multiplicação", JOptionPane.PLAIN_MESSAGE);
-		 valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Multiplicação", JOptionPane.PLAIN_MESSAGE);
+		 //valor1 = JOptionPane.showInputDialog(null, "Valor 1 : ", "Multiplicação", JOptionPane.PLAIN_MESSAGE);
+		 //valor2 = JOptionPane.showInputDialog(null, "Valor 2 : ", "Multiplicação", JOptionPane.PLAIN_MESSAGE);
 		 System.out.println("Multiplicação : (" + valor1 + " * " + valor2 + ") -> " +  calc.multiplicar(Float.parseFloat(valor1), Float.parseFloat(valor2)));
 
 	}
